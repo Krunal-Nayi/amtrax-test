ea5f2461b32cd222141c390cb4b550334f56669e
KRUNAL NAYI <krunal@krunalnayi.com>
2021-08-17 21:36:06 +1200

    Route & material
    Include material and route for Question component
    
    --taking break here

--- amtrax-test/src/app/questions/questions-routing.module.ts
+++ amtrax-test/src/app/questions/questions-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuestionMakerComponent } from './question-maker/question-maker.component';
+import { QuestionComponent } from './question/question.component';
 
 const routes: Routes = [
+  { path: 'question', component: QuestionComponent },
   { path: '', component: QuestionMakerComponent }
 ];
 
