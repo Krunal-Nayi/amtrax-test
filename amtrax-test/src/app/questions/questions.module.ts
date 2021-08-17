de1fa83fa593dac94e9d11d59a336d4dd685b652
KRUNAL NAYI <krunal@krunalnayi.com>
2021-08-17 20:24:53 +1200

    Task 2
    Create a component “question-maker” into “questions” module

--- amtrax-test/src/app/questions/questions.module.ts
+++ amtrax-test/src/app/questions/questions.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuestionsRoutingModule } from './questions-routing.module';
+import { QuestionMakerComponent } from './question-maker/question-maker.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    QuestionMakerComponent
+  ],
   imports: [
     CommonModule,
     QuestionsRoutingModule
