77e5467296a26bd5da2040c2f7b6454a58d5b30d
KRUNAL NAYI <krunal@krunalnayi.com>
2021-08-17 20:46:38 +1200

    Task 3
    Update app-routing to display “question-maker” on the http://localhost:4200/questions/

--- amtrax-test/src/app/app-routing.module.ts
+++ amtrax-test/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'questions',
+    loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsModule)
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./app.module').then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- amtrax-test/src/app/questions/questions-routing.module.ts
+++ amtrax-test/src/app/questions/questions-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionMakerComponent } from './question-maker/question-maker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: QuestionMakerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
